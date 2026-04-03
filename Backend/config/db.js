22e1ba0a85eaf018d641e5a29177aa18ee5850de
Diwyanshu kumar <diwyanshusvnit@gamil.com>
2026-04-03 15:44:24 +0530

    logger is added in db

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
-
+console.log(process.env.DATABASE_URL);
 //  Force IPv4 resolution
 const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
