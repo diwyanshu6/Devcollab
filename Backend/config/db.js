57c15174bc9dbdc5c0a17dc157b8e97bb094f0ad
Diwyanshu kumar <diwyanshusvnit@gamil.com>
2026-04-03 11:11:12 +0530

    use supabase transaction pooler

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,10 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+//  Force IPv4 resolution
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
